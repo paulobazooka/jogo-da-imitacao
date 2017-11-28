f1b7bf99a5db378adf83d1fd4f89168f55054852
Paulo Sérgio do Nascimento <paulosergionas@gmail.com>
2017-11-28 21:54:28 -0200

    criação de objeto HolderView na classe main

--- app/src/main/java/apps/poo2017/com/br/jogodaimitao/MainActivity.java
+++ app/src/main/java/apps/poo2017/com/br/jogodaimitao/MainActivity.java
@@ -1,13 +1,33 @@
 package apps.poo2017.com.br.jogodaimitao;
 
+import android.annotation.SuppressLint;
+import android.app.ActionBar;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import apps.poo2017.com.br.jogodaimitao.holderview.HolderView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private HolderView holderView = new HolderView();
+
+    @SuppressLint("ResourceAsColor")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        holderView.background = findViewById(R.id.title_panel);
+        holderView.actionBar = this.getActionBar();
+        holderView.imageButton = findViewById(R.id.imageButton);
+        holderView.textView = findViewById(R.id.textView);
+
+
     }
 }
