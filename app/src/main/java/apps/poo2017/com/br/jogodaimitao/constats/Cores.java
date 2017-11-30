f3c4f5acc2103f381df9a0d2adc73d1ebcb81b2e
Paulo Sérgio do Nascimento <paulosergionas@gmail.com>
2017-11-30 10:27:57 -0200

    criação do Enum Cores para controle seguro de escolha de cores

---
+++ app/src/main/java/apps/poo2017/com/br/jogodaimitao/constats/Cores.java
@@ -0,0 +1,19 @@
+package apps.poo2017.com.br.jogodaimitao.constats;
+
+/**
+ * Enum para controle das cores do texto
+ */
+
+public enum Cores {
+    AZUL_ESCURO("dark_blue"),
+    AZUL_CLARO("light_blue"),
+    VERMELHO("red"),
+    AMARELO("yellow"),
+    VERDE("green");
+
+    String cor;
+
+    Cores(String cor){
+        this.cor = cor;
+    }
+}
